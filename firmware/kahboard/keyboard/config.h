9c32912218b6a2f77b9d06dc9417d50a74187d15
Antoine Kahlouche <antoine@kahlouche.fr>
2023-07-02 20:07:37 -0400

    Working keyboard

--- firmware/kahboard/keyboard/config.h
+++ firmware/kahboard/keyboard/config.h
@@ -6,21 +6,7 @@
 
 #pragma once
 
-#define EE_HANDS
-
-/*
-#undef DIRECT_PINS
-#define DIRECT_PINS { \
-    { B2, D4, F6, D1, F4 }, \
-    { E6, B3, D0, F5, D3 }, \
-    { B6, D7, B1, C6, F7 }, \
-    { B4, B5, NO_PIN, NO_PIN, NO_PIN } \
-}
-#undef DIRECT_PINS_RIGHT
-#define DIRECT_PINS_RIGHT { \
-    { F4, D1, F6, D4, B2 }, \
-    { D3, F5, D0, B3, E6 }, \
-    { F7, C6, B1, D7, B6 }, \
-    { B5, B4, NO_PIN, NO_PIN, NO_PIN } \
-}
-*/
+#define EE_HAND
+#define USE_SERIAL
+#define SPLIT_MODS_ENABLE
+#define SPLIT_USB_DETECT
