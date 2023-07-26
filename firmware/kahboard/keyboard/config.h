9deb02eccdc9f5f8ad7d8725ffad9ecfb3f2be50
Antoine Kahlouche <antoine@kahlouche.fr>
2023-07-26 00:08:00 -0400

    fix combo between split

--- firmware/kahboard/keyboard/config.h
+++ firmware/kahboard/keyboard/config.h
@@ -1,13 +1,8 @@
-// Copyright 2018-2020
-// ENDO Katsuhiro <ka2hiro@curlybracket.co.jp>
-// David Philip Barr <@davidphilipbarr>
-// Pierre Chevalier <pierrechevalier83@gmail.com>
-// SPDX-License-Identifier: GPL-2.0+
-
 #pragma once
 
 #define EE_HANDS
 #define USE_SERIAL
+#define SPLIT_MODS_ENABLE
 #define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT 2000
-#define SPLIT_USB_TIMEOUT_POLL 10
+// #define SPLIT_USB_TIMEOUT 2000
+// #define SPLIT_USB_TIMEOUT_POLL 10
