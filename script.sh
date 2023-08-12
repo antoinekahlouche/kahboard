11702a96ae29b69184d060eab3b538af6371f5c6
Antoine Kahlouche <antoine@kahlouche.fr>
2023-08-12 17:41:40 -0400

    build script

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ../qmk_firmware
+
+rm -r ./keyboards/kahboard
+cp -r ../kahboard/firmware/kahboard ./keyboards
+
+qmk compile -kb kahboard -km mac
+rm ../kahboard/firmware/kahboard_mac.hex
+mv ./kahboard_mac.hex ../kahboard/firmware/kahboard_mac.hex
+
+qmk compile -kb kahboard -km win
+rm ../kahboard/firmware/kahboard_win.hex
+mv ./kahboard_win.hex ../kahboard/firmware/kahboard_win.hex
+
+cd ../kahboard
