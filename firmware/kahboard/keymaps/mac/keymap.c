06a94c176344a43c1dc14942c4def3c494fc478d
Antoine Kahlouche <antoine@kahlouche.fr>
2024-03-30 22:48:20 -0400

    update layout to output azerty on windows & accents in one keystroke

--- firmware/kahboard/keymaps/mac/keymap.c
+++ firmware/kahboard/keymaps/mac/keymap.c
@@ -36,6 +36,10 @@ bool is_gui_tab_active = false;
 bool is_ctl_tab_active = false;
 enum custom_keycodes {
     DEGREE = SAFE_RANGE,
+    A_GRAVE,
+    U_GRAVE,
+    E_AIGUE,
+    E_GRAVE,
     GUI_TAB,
     CTL_TAB,
 };
@@ -45,6 +49,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 clear_keyboard();
                 SEND_STRING(SS_LALT(SS_TAP(X_P0) SS_TAP(X_P1) SS_TAP(X_P7) SS_TAP(X_P6)));
+            }
+            break;
+        case A_GRAVE:
+            if (record->event.pressed) {
+                clear_keyboard();
+                SEND_STRING(SS_LALT(SS_TAP(X_GRV)) SS_TAP(X_A));
+            }
+            break;
+        case U_GRAVE:
+            if (record->event.pressed) {
+                clear_keyboard();
+                SEND_STRING(SS_LALT(SS_TAP(X_GRV)) SS_TAP(X_I));
+            }
+            break;
+        case E_AIGUE:
+            if (record->event.pressed) {
+                clear_keyboard();
+                SEND_STRING(SS_LALT(SS_TAP(X_K)) SS_TAP(X_K));
+            }
+            break;
+        case E_GRAVE:
+            if (record->event.pressed) {
+                clear_keyboard();
+                SEND_STRING(SS_LALT(SS_TAP(X_GRV)) SS_TAP(X_K));
             }
             break;
         case GUI_TAB:
@@ -99,13 +127,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         CO_Q, CO_W, CO_F, CO_P, CO_G,                                           CO_J, CO_L, CO_U, CO_Y, KC_QUOT, 
         CO_A, CO_R, CO_S, CO_T, CO_D,                                           CO_H, CO_N, CO_E, CO_I, CO_O,
-        CO_Z, LGUI_T(CO_X), LALT_T(CO_C), LCTL_T(CO_V), CO_B,                   CO_K, RCTL_T(CO_M), RALT_T(KC_COMM), RGUI_T(KC_DOT), S(KC_SLSH),
+        CO_Z, LGUI_T(CO_X), LALT_T(CO_C), LCTL_T(CO_V), CO_B,                   CO_K, CO_M, KC_COMM, KC_DOT, S(KC_SLSH),
         LT(_SYM, KC_TAB), LSFT_T(KC_BSPC),                                      LT(_NAV, KC_SPC), LT(_NUM, KC_ENT)
     ),
     [_SYM] = LAYOUT(
-        S(KC_2), S(KC_3), A(S(KC_2)), S(KC_4), S(KC_5),                         KC_BSLS, KC_EQL, KC_MINS, S(KC_EQL), KC_GRV,
+        S(KC_2), S(KC_3), A(CO_I), A(CO_U), S(KC_5),                            KC_BSLS, KC_EQL, KC_MINS, S(KC_EQL), KC_GRV,
         S(KC_COMM), S(KC_DOT), S(KC_9), S(KC_0), S(KC_7),                       S(KC_BSLS), S(KC_LBRC), S(KC_RBRC), KC_LBRC, KC_RBRC,
-        A(CO_U), A(CO_I), A(CO_C), A(CO_E), A(KC_GRV),                          S(KC_GRV), S(KC_MINS), KC_SLSH, S(KC_8), A(S(KC_8)),
+        A_GRAVE, U_GRAVE, A(CO_C), E_AIGUE, E_GRAVE,                            S(KC_GRV), S(KC_MINS), KC_SLSH, S(KC_8), KC_NO,
         KC_NO, KC_DEL,                                                          G(C(KC_SPC)), G(KC_SPC)
     ),
     [_NAV] = LAYOUT(
@@ -115,17 +143,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_LSFT,                                                         KC_NO, KC_NO
     ),
     [_NUM] = LAYOUT(
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                      KC_NO, KC_1, KC_2, KC_3, KC_NO,
-        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                                     KC_0, KC_4, KC_5, KC_6, KC_NO,
-        KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                                 KC_NO, KC_7, KC_8, KC_9, KC_NO,
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                      KC_NO, KC_1, KC_2, KC_3, S(KC_4),
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                                     KC_0, KC_4, KC_5, KC_6, A(S(KC_2)),
+        KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                                 A(S(KC_8)), KC_7, KC_8, KC_9, KC_NO,
         G(KC_SPC), KC_NO,                                                       KC_NO, KC_NO
     ),
 };
 
 // OVERRIDES -------------------------------------------------------------------
 
-const key_override_t comm = ko_make_basic(MOD_MASK_SHIFT, RALT_T(KC_COMM), CO_SEMICOLON); // Shift , is ;
-const key_override_t dot = ko_make_basic(MOD_MASK_SHIFT, RGUI_T(KC_DOT), S(CO_SEMICOLON)); // Shift . is :
+const key_override_t comm = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, CO_SEMICOLON); // Shift , is ;
+const key_override_t dot = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, S(CO_SEMICOLON)); // Shift . is :
 const key_override_t ques = ko_make_basic(MOD_MASK_SHIFT, KC_QUES, S(KC_1)); // Shift ? is !
 const key_override_t** key_overrides = (const key_override_t*[]) {
     &comm,
--- firmware/kahboard/keymaps/win/keymap.c
+++ firmware/kahboard/keymaps/win/keymap.c
@@ -1,50 +1,57 @@
 #include QMK_KEYBOARD_H
 
-// DEFINE COLEMAK KC IN QWERTY -------------------------------------------------
+// DEFINE COLEMAK KC IN AZERTY -------------------------------------------------
 
-#define CO_Q KC_Q
-#define CO_W KC_W
-#define CO_F KC_E
-#define CO_P KC_R
-#define CO_G KC_T
-#define CO_J KC_Y
-#define CO_L KC_U
-#define CO_U KC_I
-#define CO_Y KC_O
-#define CO_SEMICOLON KC_P
-#define CO_A KC_A
-#define CO_R KC_S
-#define CO_S KC_D
-#define CO_T KC_F
-#define CO_D KC_G
+#define CO_Q KC_A
+#define CO_W KC_Z
+#define CO_F KC_F
+#define CO_P KC_P
+#define CO_G KC_G
+#define CO_J KC_J
+#define CO_L KC_L
+#define CO_U KC_U
+#define CO_Y KC_Y
+#define CO_SEMICOLON KC_M
+#define CO_A KC_Q
+#define CO_R KC_R
+#define CO_S KC_S
+#define CO_T KC_T
+#define CO_D KC_D
 #define CO_H KC_H
-#define CO_N KC_J
-#define CO_E KC_K
-#define CO_I KC_L
-#define CO_O KC_SEMICOLON
-#define CO_Z KC_Z
+#define CO_N KC_N
+#define CO_E KC_E
+#define CO_I KC_I
+#define CO_O KC_O
+#define CO_Z KC_W
 #define CO_X KC_X
 #define CO_C KC_C
 #define CO_V KC_V
 #define CO_B KC_B
-#define CO_K KC_N
-#define CO_M KC_M
+#define CO_K KC_K
+#define CO_M KC_SEMICOLON
 
 // MACROS ----------------------------------------------------------------------
 
 bool is_alt_tab_active = false;
 bool is_ctl_tab_active = false;
 enum custom_keycodes {
-    DEGREE = SAFE_RANGE,
+    BACKTICK = SAFE_RANGE,
+    TILDE,
     ALT_TAB,
     CTL_TAB,
 };
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case DEGREE:
+        case BACKTICK:
             if (record->event.pressed) {
                 clear_keyboard();
-                SEND_STRING(SS_LALT(SS_TAP(X_P0) SS_TAP(X_P1) SS_TAP(X_P7) SS_TAP(X_P6)));
+                SEND_STRING(SS_LALT(SS_TAP(X_P0) SS_TAP(X_P9) SS_TAP(X_P6)));
+            }
+            break;
+        case TILDE:
+            if (record->event.pressed) {
+                clear_keyboard();
+                SEND_STRING(SS_LALT(SS_TAP(X_P0) SS_TAP(X_P1) SS_TAP(X_P2) SS_TAP(X_P6)));
             }
             break;
         case ALT_TAB:
@@ -97,16 +104,16 @@ enum kahboard_layers {
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        CO_Q, CO_W, CO_F, CO_P, CO_G,                                           CO_J, CO_L, CO_U, CO_Y, KC_QUOT, 
+        CO_Q, CO_W, CO_F, CO_P, CO_G,                                           CO_J, CO_L, CO_U, CO_Y, KC_4, 
         CO_A, CO_R, CO_S, CO_T, CO_D,                                           CO_H, CO_N, CO_E, CO_I, CO_O,
-        CO_Z, LCTL_T(CO_X), LALT_T(CO_C), LGUI_T(CO_V), CO_B,                   CO_K, RGUI_T(CO_M), RALT_T(KC_COMM), RCTL_T(KC_DOT), S(KC_SLSH),
+        CO_Z, LCTL_T(CO_X), LALT_T(CO_C), LGUI_T(CO_V), CO_B,                   CO_K, CO_M, CO_SEMICOLON, S(KC_COMMA), S(CO_SEMICOLON),
         LT(_SYM, KC_TAB), LSFT_T(KC_BSPC),                                      LT(_NAV, KC_SPC), LT(_NUM, KC_ENT)
     ),
     [_SYM] = LAYOUT(
-        S(KC_2), S(KC_3), ALGR(KC_5), S(KC_4), S(KC_5),                         KC_BSLS, KC_EQL, KC_MINS, S(KC_EQL), KC_GRV,
-        S(KC_COMM), S(KC_DOT), S(KC_9), S(KC_0), S(KC_7),                       S(KC_BSLS), S(KC_LBRC), S(KC_RBRC), KC_LBRC, KC_RBRC,
-        ALGR(CO_D), ALGR(CO_X), ALGR(CO_C), ALGR(CO_T), ALGR(CO_R),             S(KC_GRV), S(KC_MINS), KC_SLSH, S(KC_8), DEGREE,
-        KC_NO, KC_DEL,                                                          G(KC_DOT), A(KC_SPC)
+        ALGR(KC_0), ALGR(KC_3), KC_LBRC, S(KC_LBRC), S(KC_QUOT),                ALGR(KC_8), KC_EQL, KC_6, S(KC_EQL), BACKTICK,
+        KC_NUBS, S(KC_NUBS), KC_5, KC_MINUS, KC_1,                              ALGR(KC_6), ALGR(KC_4), ALGR(KC_EQL), ALGR(KC_5), ALGR(KC_MINUS),
+        KC_0, KC_QUOTE, KC_9, KC_2, KC_7,                                       TILDE, KC_8, S(KC_DOT), KC_BSLS, KC_NO,
+        KC_NO, KC_DEL,                                                          G(KC_COMMA), A(KC_SPC)
     ),
     [_NAV] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_PSCR, KC_PWR,                                   KC_VOLU, C(G(KC_LEFT)), ALT_TAB, CTL_TAB, C(G(KC_RIGHT)),
@@ -115,19 +122,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_LSFT,                                                         KC_NO, KC_NO
     ),
     [_NUM] = LAYOUT(
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                      KC_NO, KC_1, KC_2, KC_3, KC_NO,
-        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                                     KC_0, KC_4, KC_5, KC_6, KC_NO,
-        KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                                 KC_NO, KC_7, KC_8, KC_9, KC_NO,
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                      KC_NO, S(KC_1), S(KC_2), S(KC_3), KC_RBRC,
+        KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                                     S(KC_0), S(KC_4), S(KC_5), S(KC_6), ALGR(CO_E),
+        KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                                 S(KC_MINUS), S(KC_7), S(KC_8), S(KC_9), KC_NO,
         A(KC_SPC), KC_NO,                                                       KC_NO, KC_NO
     )
 };
 
 // OVERRIDES -------------------------------------------------------------------
 
-const key_override_t comm = ko_make_basic(MOD_MASK_SHIFT, RALT_T(KC_COMM), CO_SEMICOLON); // Shift , is ;
-const key_override_t dot = ko_make_basic(MOD_MASK_SHIFT, RCTL_T(KC_DOT), S(CO_SEMICOLON)); // Shift . is :
-const key_override_t ques = ko_make_basic(MOD_MASK_SHIFT, KC_QUES, S(KC_1)); // Shift ? is !
+const key_override_t four = ko_make_basic(MOD_MASK_SHIFT, KC_4, KC_3); // Shift ' is "
+const key_override_t comm = ko_make_basic(MOD_MASK_SHIFT, CO_SEMICOLON, KC_COMMA); // Shift , is ;
+const key_override_t dot = ko_make_basic(MOD_MASK_SHIFT, S(KC_COMMA), KC_DOT); // Shift . is :
+const key_override_t ques = ko_make_basic(MOD_MASK_SHIFT, S(CO_SEMICOLON), KC_SLASH); // Shift ? is !
 const key_override_t** key_overrides = (const key_override_t*[]) {
+    &four,
     &comm,
     &dot,
     &ques,
