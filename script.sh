e22b93b2fd07f2cdbe7546a6a074c190e56e3885
Antoine Kahlouche <antoine@kahlouche.fr>
2024-07-07 17:58:11 +0200

    remove mac and win specific

--- script.sh
+++ script.sh
@@ -9,12 +9,4 @@ qmk compile -kb kahboard -km default
 rm ../kahboard/firmware/kahboard_default.hex
 mv ./kahboard_default.hex ../kahboard/firmware/kahboard.hex
 
-qmk compile -kb kahboard -km mac
-rm ../kahboard/firmware/kahboard_mac.hex
-mv ./kahboard_mac.hex ../kahboard/firmware/kahboard_mac.hex
-
-qmk compile -kb kahboard -km win
-rm ../kahboard/firmware/kahboard_win.hex
-mv ./kahboard_win.hex ../kahboard/firmware/kahboard_win.hex
-
 cd ../kahboard
